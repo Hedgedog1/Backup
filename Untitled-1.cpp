083722d9ffe0ebe7310a13d0b14bb72cd7b4223b
Hedgedog1 <adamfaron2009@gmail.com>
2024-09-12 17:08:11 +0200

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
+
+
 bool isvalid(float value){
     float ocena[]{0.00,0.25,0.50,0.75,1.00,1.25,1.50,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,3.75,4.00,4.25,4.50,4.75,5.00,5.25,5.50,5.75,6.00};
     int size = sizeof(ocena) / sizeof(ocena[0]);
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < size; i++){
         if (value == ocena[i]){
-            return true;
+            return false; 
         }
-        return false;
     }
-    
+    return true;
 }
 
 float max(float tab[], int len){
@@ -21,6 +22,7 @@ float max(float tab[], int len){
     }
     return tmp;
 }
+
 float min(float tab[], int len){
     float tmp= tab[0];
     for(int i = 1; i < len; i++){
@@ -30,37 +32,60 @@ float min(float tab[], int len){
     }
     return tmp;
 }
+
 float srednia(float tab[], int len){
-    int tmp= 0;
+    float tmp= 0;
     for(int i = 0; i < len; i++){
         tmp += tab[i];
     }
     return tmp/len;
 }
+
 int main(){
     int liczba_ocen;
-    cout<<"Ile ocen chceszs > 4 :"<<endl;
-    cin>>liczba_ocen;
+    cout << "Ile ocen chcesz podać (więcej niż 4):" << endl;
+    cin >> liczba_ocen;
     while (liczba_ocen < 4){
-        cout<<"Ile ocen chceszs > 4";
-        cin>>liczba_ocen;
+        cout << "Liczba ocen musi być większa niż 4. Wprowadź ponownie:" << endl;
+        cin >> liczba_ocen;
     }
+
     float tab[10000];
-    cout<<"Podaj wartość liczby\n";
+    float ocena[]{0.00,0.25,0.50,0.75,1.00,1.25,1.50,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,3.75,4.00,4.25,4.50,4.75,5.00,5.25,5.50,5.75,6.00};
+    int size = sizeof(ocena) / sizeof(ocena[0]);
+    float freq[sizeof(ocena) / sizeof(ocena[0])] = {0};
+
+    cout << "Podaj wartości ocen:" << endl;
     for(int i = 0; i < liczba_ocen; i++){
-        cout<<"podaj liczbe "<< i + 1<<" : ";
-        cin>>tab[i];
+        cout << "Podaj ocenę " << i + 1 << " : ";
+        cin >> tab[i];
         if(tab[i] == 0){
             i--;
             liczba_ocen--;
             continue;
         }
         while (isvalid(tab[i])){
-            cout<<"wpisz jeszcze raz";
-            cin>>tab[i];
+            cout << "Wpisz wartość jeszcze raz (ocena musi być z listy): ";
+            cin >> tab[i];
+        }
+        for (int j = 0; j < size; j++){
+            if (tab[i] == ocena[j]){
+                freq[j]++;
+                break;
+            }
         }
     }
-    cout<<"Najmniejszy numer "<< min(tab,liczba_ocen)<<endl;
-    cout<<"Średnia "<< srednia(tab,liczba_ocen)<<endl;
-    cout<<"Największy numer "<< max(tab,liczba_ocen);
+
+    cout << "Najmniejsza ocena: " << min(tab, liczba_ocen) << endl;
+    cout << "Średnia: " << srednia(tab, liczba_ocen) << endl;
+    cout << "Największa ocena: " << max(tab, liczba_ocen) << endl;
+
+    cout << "Częstotliwość ocen:" << endl;
+    for (int i = 0; i < size; i++){
+        if (freq[i] > 0){
+            cout << ocena[i] << ": " << freq[i] << " razy" << endl;
+        }
+    }
+
+    return 0;
 }
